1e6f8cda70971993dbe606a32b195ffb93043efd
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-04-07 11:48:29 +0300

    refactor: add thr_id parameter to texture binding methods
    Updated bindtexture_1D and bindtexture_2D signatures to include thread ID parameter for proper device context handling.

--- scrypt/nv_kernel.h
+++ scrypt/nv_kernel.h
@@ -11,8 +11,8 @@ public:
 	virtual void set_scratchbuf_constants(int MAXWARPS, uint32_t** h_V);
 	virtual bool run_kernel(dim3 grid, dim3 threads, int WARPS_PER_BLOCK, int thr_id, cudaStream_t stream, uint32_t* d_idata, uint32_t* d_odata, unsigned int N, unsigned int LOOKUP_GAP, bool interactive, bool benchmark, int texture_cache);
 
-	virtual bool bindtexture_1D(uint32_t *d_V, size_t size);
-	virtual bool bindtexture_2D(uint32_t *d_V, int width, int height, size_t pitch);
+	virtual bool bindtexture_1D(uint32_t *d_V, size_t size, int thr_id);
+	virtual bool bindtexture_2D(uint32_t *d_V, int width, int height, size_t pitch, int thr_id);
 	virtual bool unbindtexture_1D();
 	virtual bool unbindtexture_2D();
 
